aebd495e38fddb327df1dc530aebf335248cedc7
Marcel Berlin <berlin_m@gmx.de>
2023-04-09 20:03:18 +0200

    firebase collection link added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject  } from '@angular/core';
+import { Firestore, collection, collectionData  } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ring-of-fire';
+  firestore: Firestore = inject(Firestore);
+  games$: Observable<any[]>;
+
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'games')
+    this.games$ = collectionData(aCollection);
+  }
 }
