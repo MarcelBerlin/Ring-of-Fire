bf85330b3245aca6cc92118070bd4fc59320e3e0
Marcel Berlin <berlin_m@gmx.de>
2023-04-09 18:55:23 +0200

    firebase integrated

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-31fd6',
+    appId: '1:701472008847:web:1ba1424886d2425ac4defa',
+    storageBucket: 'ring-of-fire-31fd6.appspot.com',
+    apiKey: 'AIzaSyDr1Kh9PFPjGRLGGQqe71LFbHvvqK0k4kU',
+    authDomain: 'ring-of-fire-31fd6.firebaseapp.com',
+    messagingSenderId: '701472008847',
+  },};
