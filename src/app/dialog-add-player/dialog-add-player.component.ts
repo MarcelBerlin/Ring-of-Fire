1d7c246f156a78cb0060bf5a7b61b057d6fda76d
Marcel Berlin <berlin_m@gmx.de>
2023-04-23 15:07:36 +0200

    edit player fixed #2

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef} from '@angular/material/dialog';
 
 
 @Component({
@@ -10,11 +10,24 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class DialogAddPlayerComponent implements OnInit {
 
-  name: string="";
+  player: FormGroup;
+  profiles: string[] = [
+    'profile_men_1.png',
+    'profile_men_2.png',
+    'profile_woman_1.png',
+    'profile_woman_2.png'    
+  ];
+
+  name: string;
+  profile: string;
+
   constructor(private dialogRef: MatDialogRef<DialogAddPlayerComponent>) {}
 
   ngOnInit(): void {
-    
+    this.player = new FormGroup({
+      name: new FormControl('', Validators.required),
+      profile: new FormControl('', Validators.required)
+    })
   }
 
   onNoClick(): void {
