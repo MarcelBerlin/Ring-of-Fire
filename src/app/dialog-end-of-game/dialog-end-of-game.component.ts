741a74938bbdbd500e3299dfd97a4bf20494a283
Marcel Berlin <berlin_m@gmx.de>
2023-04-23 15:01:55 +0200

    edit player fixed

--- src/app/dialog-end-of-game/dialog-end-of-game.component.ts
+++ src/app/dialog-end-of-game/dialog-end-of-game.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dialog-end-of-game',
   templateUrl: './dialog-end-of-game.component.html',
   styleUrls: ['./dialog-end-of-game.component.scss']
 })
-export class DialogEndOfGameComponent {
+export class DialogEndOfGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
